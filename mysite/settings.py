57ed756d05a6c646eb4536ae332c0badecd8e3a4
ryks4lyfe <rykerynwa@gmail.com>
2023-04-12 11:44:55 -0400

    Tutor Profile Update + Nav Bar

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,12 +136,13 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SITE_ID = 13
+SITE_ID = 11
 
 AUTH_USER_MODEL = 'tutorme.AppUser'
 
